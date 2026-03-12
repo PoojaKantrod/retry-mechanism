7ace36506bf4768ebd99dc47f790b3948111e671
PoojaKantrod <114088185+PoojaKantrod@users.noreply.github.com>
2026-03-12 18:45:07 -0500

    Delete src/main/java/com/example/payment directory

--- src/main/java/com/example/payment/exception/PaymentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.payment.exception;
-
-public class PaymentNotFoundException extends RuntimeException {
-
- public PaymentNotFoundException(String message) {
-  super(message);
- }
-
-}
